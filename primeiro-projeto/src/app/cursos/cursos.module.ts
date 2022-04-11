2a5362b381de2113ee33ddcf23973c0fe11a34a7
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-11 15:46:21 -0300

    Feat: Criado o componente cursos pelo CLI

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { CursosComponent } from './cursos.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    CursosComponent
+  ],
   imports: [
     CommonModule
   ]
