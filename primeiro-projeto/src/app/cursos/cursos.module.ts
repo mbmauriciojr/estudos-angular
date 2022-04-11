921ff959d40ec3db1d4de778399185c5a739e58d
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-11 16:06:09 -0300

    Feat: Criado um novo componente dentro do módulo cursos

--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { CursosComponent } from './cursos.component';
+import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
 
 
 @NgModule({
   declarations: [
-    CursosComponent
+    CursosComponent,
+    CursoDetalheComponent
   ],
   imports: [
     CommonModule
