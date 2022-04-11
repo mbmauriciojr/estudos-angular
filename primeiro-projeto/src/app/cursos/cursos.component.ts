d3651eaaf7e68f8b360a7f82ccacb33f5f759d67
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-11 16:46:54 -0300

    Feat: Criado um exemplo de interpolacao em elemento HTML do Angular

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -7,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  constructor() { 
+    this.nomePortal = 'http://loiane.training';
+  }
 
   ngOnInit(): void {
   }
