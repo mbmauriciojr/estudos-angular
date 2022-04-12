428f7192119ab50ac14f9614c09a471da1c84dd0
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-12 10:49:15 -0300

    Fix: Colocado manualmente a importação para o bootstrap funcionar

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,7 +13,8 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    TooltipModule.forRoot(),
   ],
   providers: [],
   bootstrap: [AppComponent]
