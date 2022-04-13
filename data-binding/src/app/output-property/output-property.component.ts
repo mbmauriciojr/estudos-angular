86ec2e4396c66b5bbb9ef290f432026fecbce177
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-13 14:48:57 -0300

    Fix: Agora o valor inicial do contador vem através de property binding

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'contador',
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutputPropertyComponent implements OnInit {
 
-  valor: number = 0;
+  @Input() valor: number = 0;
 
   incrementa() {
     this.valor ++;
