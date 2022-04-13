4629a2465806a32d1c3343f4e269ba5c119da171
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-13 16:14:58 -0300

    Feat: Inserido a diretiva ngIf para interacao de uma destruicao de componente

--- data-binding/src/app/app.component.ts
+++ data-binding/src/app/app.component.ts
@@ -9,7 +9,14 @@ export class AppComponent {
   
   valor: number = 5;
 
+  deletarCiclo: boolean = false;
+
   mudarValor() {
     this.valor ++;
   }
+
+  destruirCiclo() {
+    this.deletarCiclo = true;
+  }
+
 }
