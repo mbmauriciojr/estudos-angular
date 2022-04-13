a93f23deb026d00a1028d9efa75f36fa3d95cddd
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-13 15:53:49 -0300

    Feat: Colocado um log em cada fase do Cycle com as Interfaces correspondentes

--- data-binding/src/app/ciclo/ciclo.component.ts
+++ data-binding/src/app/ciclo/ciclo.component.ts
@@ -1,15 +1,61 @@
-import { Component, OnInit } from '@angular/core';
+import { 
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  OnChanges,
+  OnDestroy,
+  OnInit
+} from '@angular/core';
 
 @Component({
   selector: 'app-ciclo',
   templateUrl: './ciclo.component.html',
   styleUrls: ['./ciclo.component.css']
 })
-export class CicloComponent implements OnInit {
+export class CicloComponent implements OnChanges, OnInit, DoCheck,
+  AfterContentInit, AfterContentChecked, AfterViewInit, AfterViewChecked, OnDestroy {
 
-  constructor() { }
+  constructor() { 
+    this.log('constructor');
+  }
 
-  ngOnInit(): void {
+  ngOnChanges() {
+    this.log('ngOnChanges');
+  }
+
+  ngOnInit() {
+    this.log('ngOnInit');
+  }
+
+  ngDoCheck() {
+    this.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    this.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    this.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    this.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    this.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    this.log('ngOnDestroy');
+  }
+
+  private log(hook: string) {
+    console.log(hook);
   }
 
 }
