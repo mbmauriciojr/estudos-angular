6933e9047a0b959937f0e40a5ad88fdcd639516e
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-13 15:27:22 -0300

    Feat: Criado o componente Ciclo para o estudo de Life Cycle de componentes

---
+++ data-binding/src/app/ciclo/ciclo.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ciclo',
+  templateUrl: './ciclo.component.html',
+  styleUrls: ['./ciclo.component.css']
+})
+export class CicloComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
