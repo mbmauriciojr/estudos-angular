643b4e06ffceca9ce870b941bdb8d11a088bd8a9
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-13 10:10:25 -0300

    Feat: Criado o componente inpu-property

---
+++ data-binding/src/app/input-property/input-property.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-curso',
+  templateUrl: './input-property.component.html',
+  styleUrls: ['./input-property.component.css']
+})
+export class InputPropertyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
