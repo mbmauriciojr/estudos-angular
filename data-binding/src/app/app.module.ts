836012aea6d932120438f575978d5da13ea7153a
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-13 09:13:11 -0300

    Fix: Foi importado o modulo de Forms pois somente assim o ngModel funcionou

--- data-binding/src/app/app.module.ts
+++ data-binding/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { TooltipModule } from 'ngx-bootstrap/tooltip';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -15,6 +16,7 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
     BrowserModule,
     AppRoutingModule,
     TooltipModule.forRoot(),
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
