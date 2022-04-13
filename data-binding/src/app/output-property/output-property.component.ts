c7ffcb8387f0e9494450b0f2f09f410638b50752
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-13 14:38:30 -0300

    Feat: Criado os botoes do contador e a variavel a ser manipulada no componente

--- data-binding/src/app/output-property/output-property.component.ts
+++ data-binding/src/app/output-property/output-property.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OutputPropertyComponent implements OnInit {
 
+  valor: number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
