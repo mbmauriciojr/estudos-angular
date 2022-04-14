59771a7a7dad1758862c87ba45a9f617512be583
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-14 09:16:17 -0300

    Feat: Criado um componente através da CLI

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DiretivaNgifComponent
   ],
   imports: [
     BrowserModule,
