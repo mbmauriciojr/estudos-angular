875cc372c77afc80e24a182f4bb79dcd0893eb71
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-14 10:29:22 -0300

    Fix: Mudado os arquivos para utilizacao correta e adicionado regra no eslint

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-diretiva-ngif',
   templateUrl: './diretiva-ngif.component.html',
-  styleUrls: ['./diretiva-ngif.component.css']
+  styleUrls: ['./diretiva-ngif.component.scss']
 })
 export class DiretivaNgifComponent implements OnInit {
 
