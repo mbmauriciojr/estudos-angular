57bf85161abd066e059b70dfb47bb5e7843746b0
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-15 11:16:23 -0300

    Feat: Exemplo basico utilizando ngFor em um array

--- diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
+++ diretivas/src/app/diretiva-ngfor/diretiva-ngfor.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgforComponent implements OnInit {
 
+  cursos: string[] = ['Angular', 'HTML', 'CSS', 'JavaScript'];
+
   constructor() { }
 
   ngOnInit(): void {
