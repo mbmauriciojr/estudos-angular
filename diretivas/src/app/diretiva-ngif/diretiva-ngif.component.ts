19b5805dd8d94be976c27b229ff6930349521c0a
Marcelo Maurício <mmauricio.design@gmail.com>
2022-04-15 09:52:11 -0300

    Feat: Criado um exemplo utilizando uma variavel do tipo boolean

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
-  cursos: string[] = [];
+  cursos: string[] = ['Angular'];
+
+  mostrarCursos: boolean = false;
 
   constructor() { }
 
