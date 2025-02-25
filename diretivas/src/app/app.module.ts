d74696ec1fcde37edd593f0c69171a04cc3c136b
Marcelo Benjamin Maurício Junior <mmauricio.design@gmail.com>
2025-02-25 19:46:01 -0300

    feat(angular-diretivas): diretivas com input e property binding

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -11,6 +11,7 @@ import { ExemploNgContentComponent } from './exemplo-ng-content/exemplo-ng-conte
 import { FundoAmareloDirective } from './shared/fundo-amarelo.directive';
 import { DiretivasCustomizadasComponent } from './diretivas-customizadas/diretivas-customizadas.component';
 import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
+import { HighlightDirective } from './shared/highlight.directive';
 
 @NgModule({
   declarations: [
@@ -21,7 +22,8 @@ import { HighlightMouseDirective } from './shared/highlight-mouse.directive';
     ExemploNgContentComponent,
     FundoAmareloDirective,
     DiretivasCustomizadasComponent,
-    HighlightMouseDirective
+    HighlightMouseDirective,
+    HighlightDirective
   ],
   imports: [
     BrowserModule,
